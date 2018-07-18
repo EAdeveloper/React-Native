0f58718a8002935f7888eaa67d5695b4a48ad457
Edward Almanzar <eddeveloper@hotmail.com>
2018-07-18 16:51:29 -0400

    ScroolView- inefficient for long list

--- rn-course/src/components/PlaceList/PlaceList.js
+++ rn-course/src/components/PlaceList/PlaceList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet } from 'react-native';
+import {View, StyleSheet, ScrollView } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
@@ -12,9 +12,9 @@ const placeList = props =>{
         />
       ));
     return(
-        <View style={styles.ListContainer}>
+        <ScrollView style={styles.ListContainer}>
           {placesOutput}
-        </View>
+        </ScrollView>
     );
 };
 
