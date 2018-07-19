e5f8a99dc9a50b7c85ecda1c1862be5e6c8ce105
Edward Almanzar <eddeveloper@hotmail.com>
2018-07-18 20:08:35 -0400

    Adding static Images

--- rn-course/App.js
+++ rn-course/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
+import placeImage from './src/assets/beautifull.jpg';
 
 
 export default class App extends React.Component {
@@ -18,7 +19,9 @@ placeAddedHandler = placeName =>{
     return {
       places: prevState.places.concat({
         key: Math.random().toString(), 
-        value: placeName})
+        value: placeName,
+        image: placeImage
+      })
     }
   });
 };
